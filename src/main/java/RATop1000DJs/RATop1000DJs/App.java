f1fb4f2656edb200c0156c3dd6cbe86941defcce
Bernard Cooley <bernardcooley@gmail.com>
2016-10-02 00:54:02 +0100

    first commit

---
+++ src/main/java/RATop1000DJs/RATop1000DJs/App.java
@@ -0,0 +1,13 @@
+package RATop1000DJs.RATop1000DJs;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
